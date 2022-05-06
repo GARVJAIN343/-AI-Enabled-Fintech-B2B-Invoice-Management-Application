e025d4a949bc66fd1c61e8a5d6f159866a73c9cb
GARVJAIN343 <garvjain343@gmail.com>
2022-05-06 19:20:41 +0530

    Updates

---
+++ precache-manifest.adeb75a91714dca558d676c9b7fd1546.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb94c8add8699bc73478",
+    "url": "/B2B-Invoice-Management-Application/static/css/main.b20deab3.chunk.css"
+  },
+  {
+    "revision": "eb94c8add8699bc73478",
+    "url": "/B2B-Invoice-Management-Application/static/js/main.610354da.chunk.js"
+  },
+  {
+    "revision": "ffaf765867f2b5714501",
+    "url": "/B2B-Invoice-Management-Application/static/js/runtime~main.3e4228cb.js"
+  },
+  {
+    "revision": "bb489715a091679a60ad",
+    "url": "/B2B-Invoice-Management-Application/static/js/2.040c92c2.chunk.js"
+  },
+  {
+    "revision": "bd17f23c3380b7c64145",
+    "url": "/B2B-Invoice-Management-Application/static/js/3.6b5b8207.chunk.js"
+  },
+  {
+    "revision": "77abd49565a62fae3531",
+    "url": "/B2B-Invoice-Management-Application/static/js/4.d59669e1.chunk.js"
+  },
+  {
+    "revision": "7c1fda79d9d5645abf28",
+    "url": "/B2B-Invoice-Management-Application/static/js/5.4e89c5f4.chunk.js"
+  },
+  {
+    "revision": "612f37ecfa6cac5665d35216c5f5728d",
+    "url": "/B2B-Invoice-Management-Application/static/media/logo1.612f37ec.png"
+  },
+  {
+    "revision": "5aeefa8b71f6fe21637a9f7eb4e1d9cc",
+    "url": "/B2B-Invoice-Management-Application/index.html"
+  }
+];
